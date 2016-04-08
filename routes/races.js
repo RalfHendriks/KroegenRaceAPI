e1d9ffca073186be7981e81262023864d836efb7
Ralf Hendriks <rjhendri@avans.nl>
2016-04-08 04:23:30 +0200

    afsasf

--- routes/races.js
+++ routes/races.js
@@ -2,12 +2,15 @@ var express = require('express');
 var router = express.Router();
 var _ = require('underscore');
 var https = require('https');
+var async = require('async');
+var authorization = require('../config/authorization');
+var auth = new authorization();
 var Race;
 var User;
 var Bar;
 
 router.route('/')
-    .get(loadMain)
+    .get(getRaces)
     .post(addRace);
     
 router.route('/:id')
@@ -38,36 +41,63 @@ module.exports = function(race,user,bar) {
     return router;
 };
 
-function loadMain(req, res){
-     getPlaces('51.274651', '5.575767', function(place){
-        place.data.results.forEach(function(googlePlacesBar) {
-            var newBar = new Bar();
-            newBar.location.lat = googlePlacesBar.geometry.location.lat;
-            newBar.location.long = googlePlacesBar.geometry.location.lng;
-            newBar.name = googlePlacesBar.name+'asfasf';
-            Bar.findOne({'name' : newBar.name} ,function (err,bar) {
-                if(bar == null){
-                    newBar.save(function(err,newSavedBar){
-                        console.log(err);
-                    });
-                }
+function getRaces(req, res){
+    var permission = auth.validAction(req.user);
+    if(permission == '1'){
+        Race.find({}, function(err, races) {
+            var raceMap = [];
+            races.forEach(function(race) {
+                raceMap.push(race);
             });
-        });  
+            console.log(raceMap);
+            res.render('race', {races: raceMap,userPermission: permission });  
+        });
+    }
+    else{
+        res.render('404');  
+    }
+}
+
+function getBar(name,callback){
+    var selectedBar;
+    Bar.findOne({'name' : name} ,function (err,bar) {
+        if(bar == null){
+        newBar.save(function(err,newSavedBar){
+            selectedBar = newSavedBar;
+            });
+        }
+        else
+        {
+            selectedBar = bar;
+        }
+        callback({selectedBar});
+    });
+}
+
+function getBarPLaces(newRace,callback){
+        getPlaces('51.274651', '5.575767', function(bars){
+            bars.object.results.forEach(function(googlePlacesBar) {
+                var newBar = new Bar();
+                newBar.location.lat = googlePlacesBar.geometry.location.lat;
+                newBar.location.long = googlePlacesBar.geometry.location.lng;
+                newBar.name = googlePlacesBar.name;
+                getBar(newBar.name,function(result){
+                    newRace.bars.push(result);
+                    newRace.save();
+                });
+            });
+            callback({newRace});
     });
 }
 
 function addRace(req, res){
-
-    /*var newRace = new Race(req.body);
-    console.log(newRace);
-    newRace.save(function(err, savedRace){
-		if(err){
-            res.json(err);
-         }
-		else {
-			res.json(newRace);
-		}
-	});*/
+    
+    var newRace = new Race(req.body);
+        getBarPLaces(newRace,function(barResults){
+            newRace = barResults;
+            
+            res.json(newRace);   
+        });
 }
 
 function addUser(req,res){
@@ -97,10 +127,6 @@ function addBar(req,res){
 }
 
 function removeUser(req,res){
-    var url = require('url');
-var url_parts = url.parse(request.url, true);
-var query = url_parts.query;
-console.log(query);
     var query = getRequestId(req);
     Race.findOne(query,function (err,race) {
         var i = race.users.indexOf(req.params.userid);
@@ -164,10 +190,10 @@ function getPlaces(lat, long, callback){
         // Als het hele response terug is kunnen we verder
         response.on('end', function () {
             var object = JSON.parse(content);
-            // De callback nu uitvoeren
             callback({
-                data : object
+                object
             });
         });
     });
 }
+
