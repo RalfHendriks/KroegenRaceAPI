60c3db76cfd043c8abb75f4d3e89cab89b250127
patrickleijser <patrick@breaky.eu>
2016-06-24 10:17:07 +0200

    tests updated

--- test/test.js
+++ test/test.js
@@ -3,6 +3,9 @@ var app = require('../app');
 var request = require('supertest');
 var server = request.agent(app);
 
+var expect = require('chai').expect;
+var should = require('chai').should();
+
 // Functions
 function loginUser() {
     return function(done) {
@@ -42,7 +45,7 @@ describe('Authentication', function(){
     it('Should_ResponseHtml_When_LoggedInAndOpeningRacesPage', function(done){
         server
             .get('/races')
-            .set('Accept', 'text/html')
+            .set('Content-Type', 'text/html')
             .expect(200)
             .expect('Content-Type', /html/)
             .end(function(err, res){
@@ -56,11 +59,13 @@ describe('Authentication', function(){
     it('Should_ResponseJson_When_LoggedOutAndOpeningRacesPage', function(done){
         server
             .get('/races')
-            .set('Accept', 'text/html')
+            .set('Content-Type', 'text/html')
             .expect(200)
             .expect('Content-Type', /json/)
             .end(function(err, res){
                 if (err) return done(err);
+
+                expect(res.body).to.equal('Permission denied!');
                 done()
             });
     });
@@ -70,24 +75,32 @@ describe('Routing', function(){
     it('Should_ReturnOK_When_OpeningRaces', function(done){
         server
             .get('/races')
-            .set('Accept', 'application/json')
+            .set('Content-Type','application/json')
             .expect(200)
             .expect('Content-Type', /json/)
             .end(function(err, res){
                 if (err) return done(err);
+
+                expect(res.body[0]).to.not.be.undefined;
+                expect(res.body[0]).to.have.property('_id');
                 done()
             });
     });
 
     it('Should_ReturnOK_When_OpeningSingleRace', function(done){
+
         server
-            .get('/races/fakeid')
-            .set('Accept', 'application/json')
+            .get('/races/576be0926b8cf8f03e1a1703')
+            .set('Content-Type','application/json')
             .expect(200)
             .expect('Content-Type', /json/)
             .end(function(err, res){
                 if (err) return done(err);
-                done(res)
+
+                expect(res.body).to.have.property('_id');
+                expect(res.body._id).to.be.a('String');
+                expect(res.body.bars).to.not.be.empty;
+                done()
             });
     });
 
