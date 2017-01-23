c125312c17402fb03d26576d756f8a1374c76229
Ralf Hendriks <rjhendri@avans.nl>
2017-01-23 14:08:14 +0100

    config update

--- config/development.js
+++ config/development.js
@@ -8,17 +8,17 @@ config.googleplaces = {
 config.facebookAuth = {
         'clientID'      : '600322860116047',
         'clientSecret'  : '080f432b8098bc46565de184bb16f934',
-        'callbackURL'   : 'http://localhost:5000/auth/facebook/callback'
+        'callbackURL'   : 'https://kroegen-race-api.herokuapp.com/auth/facebook/callback'
     },
 config.googleAuth = {
 		'clientID' 		: '1057465675223-t3s3ub3d0e77o0hh3uq1rv8a9hcucf8d.apps.googleusercontent.com',
 		'clientSecret' 	: 'h5UI6G2kYD82IZwfhgfLhdRH',
-		'callbackURL' 	: 'http://localhost:5000/auth/google/callback'
+		'callbackURL' 	: 'https://kroegen-race-api.herokuapp.com/auth/google/callback'
 	}
 
 config.server = {
   host: '0.0.0.0',
-  port: process.env.PORT || 5000
+  port: process.env.PORT || 3000
 };
 
 config.mlab = {
